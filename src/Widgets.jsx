283c84a6ada90592fa87d05a1cf5b4eb4fc611c4
Dmitry Fiodorov <d.fiodorov@vironit.co.uk>
2019-08-13 17:56:22 +0300

    init

---
+++ src/Widgets.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Widgets extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.app_type) {
+      window.app_type = props.app_type;
+    }
+  }
+
+  componentDidMount() {
+    Widgets.appendStyle();
+    Widgets.appendScript();
+  }
+
+  static appendStyle() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/style-min.css';
+
+    document.head.appendChild(link);
+  }
+
+  static appendScript() {
+    const script = document.createElement('script');
+    script.src = '/bzcalc-core-min.js';
+    script.async = true;
+    script.onload = Widgets.scriptLoaded;
+
+    document.body.appendChild(script);
+  }
+
+  static scriptLoaded() {
+    window.app_type = 'd8';
+    window.bzHome();
+  }
+
+  render() {
+    return <div className="main-container container" id="bzface"
+                role="main">&nbsp;</div>;
+  }
+}
+
+export default Widgets;
